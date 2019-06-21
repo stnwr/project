d17f12f2c8af1b097628084cb50b5ba317e56562
David Stone <davidjamesstone@gmail.com>
2019-06-21 20:15:58 +0100

    Update

---
+++ db/nearlife-migrations/20190407231245_initial.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const { createDB } = require('@stoneware/db')
+const app = require('../../nearlife.json')
+const relativeTo = path.join(__dirname, '../..')
+
+exports.up = function (knex) {
+  return createDB(app, knex, { relativeTo })
+}
+
+exports.down = function (knex) {
+
+}
--- knexfile.js
+++ knexfile.js
@@ -23,6 +23,29 @@ module.exports = {
     seeds: {
       directory: './db/seeds'
     }
+  },
+  nearlife: {
+    client: 'mysql2',
+    connection: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: 'Babble01',
+      database: 'nearlife',
+      typeCast (field, next) {
+        // Convert 1 to true, 0 to false, and leave null alone
+        if (field.type === 'TINY' && field.length === 1) {
+          const value = field.string()
+          return value ? value === '1' : null
+        }
+        return next()
+      }
+    },
+    migrations: {
+      directory: './db/nearlife-migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
+    }
   }
   // ,
 
